8ba65c52df4b546c5270d721161ea9c6e2bff296
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-01-30 20:30:06 -0300

    feat: auth-employee-service-controller-complete

--- backend/src/services/employee/AuthEmployeeService.ts
+++ backend/src/services/employee/AuthEmployeeService.ts
@@ -1,9 +1,47 @@
+import prismaClient from "../../prisma";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
 interface AuthEmployeeRequest {
-    
+    email: string;
+    senha: string;
 }
 
 export class AuthEmployeeService {
-    async execute() {
+    async execute({ email, senha }: AuthEmployeeRequest) {
+        const employee = await prismaClient.funcionario.findFirst({
+            where: {
+                email: email,
+            },
+        });
 
+        if (!employee) {
+            throw new Error("[ERROR] Email/Senha incorreto(a)!");
+        }
+
+        const passwordMatch = await compare(senha, employee.senha); 
+
+        if (!passwordMatch) {
+            throw new Error("[ERROR] Email/Senha incorreto(a)!");
+        }
+
+        const token = sign(
+            {
+                name: employee.nome,
+                email: employee.email,
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: employee.id,
+                expiresIn: '30d'
+            }
+        );
+
+        return {
+            id: employee.id,
+            nome: employee.nome,
+            email: employee.email,
+            token: token,
+        }
     }
 }
