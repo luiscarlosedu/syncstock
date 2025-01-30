beb27d452a7e3c9010703154aa8df295c0290221
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-01-30 15:50:24 -0300

    feat: Service and controller created

---
+++ backend/src/controllers/employee/AuthEmployeeController.ts
@@ -0,0 +1,5 @@
+export class AuthEmployeeController {
+    async handle() {
+        
+    }
+}
---
+++ backend/src/services/employee/AuthEmployeeService.ts
@@ -0,0 +1,12 @@
+interface AuthEmployeeRequest {
+    nome: string;
+    email: string;
+    senha: string;
+    foto?: string;
+}
+
+export class AuthEmployeeService {
+    async execute() {
+
+    }
+}
