7db428f3dd7049b4816a4807e62166ae0f59986d
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-02-03 15:29:48 -0300

    fix: remove-acess-token

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,14 +6,12 @@ import uploadConfig from './config/multer';
 import { isAuthenticaded } from "./middlewares/isAuthenticaded";
 
 import { CreateEnterpriseController } from "./controllers/enterprise/CreateEnterpriseController";
-import { GetEnterpriseAcessTokenController } from "./controllers/enterprise/GetEnterpriseAcessTokenController";
 import { AuthEnterpriseController } from "./controllers/enterprise/AuthEnterpriseController";
 import { GetDetailEnterpriseController } from "./controllers/enterprise/GetDetailEnterpriseController";
 import { ListEmployeesController } from "./controllers/enterprise/ListEmployeesController";
 import { RemoveEmployeeController } from "./controllers/enterprise/RemoveEmployeeController";
 
 import { CreateEmployeeController } from "./controllers/employee/CreateEmployeeController";
-import { AuthTokenAcessEmployeeController } from './controllers/employee/AuthTokenAcessEmployeeController';
 import { AuthEmployeeController } from "./controllers/employee/AuthEmployeeController";
 import { EmployeeDetailController } from "./controllers/employee/EmployeeDetailController";
 import { EmployeeDetailEnterpriseController } from "./controllers/employee/EmployeeDetailEnterpriseController";
@@ -41,13 +39,11 @@ router.post("/employee/session", new AuthEmployeeController().handle);
 // rotas - Auth
 
 // Rotas - Enterprise / Empresa
-router.get("/enterprise/token", isAuthenticaded, new GetEnterpriseAcessTokenController().handle);
 router.get("/enterprise/detail", isAuthenticaded, new GetDetailEnterpriseController().handle);
 router.get("/enterprise/employees", isAuthenticaded, new ListEmployeesController().handle);
 router.patch("/enterprise/remove-employee", isAuthenticaded, new RemoveEmployeeController().handle);
 
 // Rotas - Employee / Funcionário
-router.post("/employee/acess-token", isAuthenticaded, new AuthTokenAcessEmployeeController().handle);
 router.get("/employee/detail", isAuthenticaded, new EmployeeDetailController().handle);
 router.get("/employee/enterprise", isAuthenticaded, new EmployeeDetailEnterpriseController().handle);
 
