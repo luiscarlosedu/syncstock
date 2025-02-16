dee86eca29b0a6d79f5e7fdbae1b37e7fc4a540b
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-02-16 16:11:31 -0300

    feat: login-enterprise-form

--- frontend/src/pages/AuthEnterprise/LoginEnterprise/index.tsx
+++ frontend/src/pages/AuthEnterprise/LoginEnterprise/index.tsx
@@ -1,6 +1,18 @@
 import {
     Container,
+    LoginContainer,
     LoginContent,
+    LoginContentText,
+    LoginTitle,
+    LoginSubTitle,
+    LoginForm,
+    LoginInputContainer,
+    LoginLabel,
+    Input,
+    LoginFormSubmit,
+    HorizontalRow,
+    Hr,
+    HorizontalRowText
 } from "./styles";
 
 import { AuthHeader } from "../../../components/auth-header";
@@ -8,9 +20,63 @@ import { AuthHeader } from "../../../components/auth-header";
 export default function LoginEnterprise() {
     return (
         <Container>
-            <LoginContent>
+            <LoginContainer>
                 <AuthHeader />
-            </LoginContent>
+                <LoginContent>
+                    <LoginContentText>
+                        <LoginTitle>Entrar como Empresa</LoginTitle>
+                        <LoginSubTitle>Que bom te ver de novo!</LoginSubTitle>
+                    </LoginContentText>
+
+                    <LoginForm>
+                        <LoginInputContainer>
+                            <LoginLabel htmlFor="iemail">Email</LoginLabel>
+                            <Input 
+                                placeholder="testeteste@gmail.com"
+                                type="email"
+                                required
+                                id="iemail"
+                            />
+                        </LoginInputContainer>
+
+                        <LoginInputContainer>
+                            <LoginLabel htmlFor="icnpj">CNPJ</LoginLabel>
+                            <Input 
+                                placeholder="12.345.678/0001-90"
+                                type="text"
+                                required
+                                id="icnpj"
+                                maxLength={18}
+                                minLength={14}
+                            />
+                        </LoginInputContainer>
+
+                        <LoginInputContainer>
+                            <LoginLabel htmlFor="isenha">Senha</LoginLabel>
+                            <Input 
+                                placeholder="***************"
+                                type="password"
+                                required
+                                id="isenha"
+                            />
+                        </LoginInputContainer>
+
+                        <LoginFormSubmit 
+                            type="submit"
+                        >
+                            Entrar
+                        </LoginFormSubmit>
+
+                    </LoginForm>
+
+                    <HorizontalRow>
+                        <Hr />
+                        <HorizontalRowText>ou</HorizontalRowText>
+                        <Hr />
+                    </HorizontalRow>
+
+                </LoginContent>
+            </LoginContainer>
         </Container>
     );
 };
--- frontend/src/pages/AuthEnterprise/LoginEnterprise/styles.ts
+++ frontend/src/pages/AuthEnterprise/LoginEnterprise/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
     width: 100%;
 `;
 
-export const LoginContent = styled.div`
+export const LoginContainer = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
@@ -14,25 +14,85 @@ export const LoginContent = styled.div`
     align-items: center;
 `;
 
-export const LoginContentHeader = styled.div`
+export const LoginContent = styled.div`
     width: 100%;
+    height: 100%;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    padding: 20px;
+    gap: 20px;
 `;
 
-export const HeaderLogo = styled.div`
+export const LoginContentText = styled.div`
+    
+`;
+
+export const LoginTitle = styled.h1`
+    font-size: 27px;
+`;
+
+export const LoginSubTitle = styled.h2`
+    font-size: 19px;
+    color: #606060;
+`;
+
+export const LoginForm = styled.form`
+    width: 100%;
+`;
+
+export const LoginInputContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
+`;
+
+export const LoginLabel = styled.label`
+    font-weight: 600;
+    font-size: 15px;
+`;
+
+export const Input = styled.input`
+    padding: 10px 10px;
+    border: 1.5px solid black;
+    font-size: 14px;
+    border-radius: 5px;
+    margin-bottom: 10px;
+`;
+
+export const LoginFormSubmit = styled.button`
+    background-color: #323232;
+    color: white;
+    width: 100%;
+    height: 45px;
+    border: 0;
+    outline: none;
+    cursor: pointer;
+    border-radius: 5px;
+    font-size: 14px;
+    transition: 0.2s;
+    user-select: none;
+
+    &:hover {
+        background-color: #505050;
+    }
+`;
+
+export const HorizontalRow = styled.div`
     display: flex;
     align-items: center;
+    justify-content: center;
+    width: 100%;
     gap: 10px;
 `;
 
-export const LoginHeaderTitle = styled.h1`
-    font-size: 25px;
+export const Hr = styled.hr`
+    flex: 1;
+    border: none;
+    border-top: 1px solid #323232;
 `;
 
-export const LoginHeaderImage = styled.img`
-    width: 30px;
-    height: 30px;
+export const HorizontalRowText = styled.h3`
+    font-size: 14px;
+    font-weight: 500;
 `;
-
