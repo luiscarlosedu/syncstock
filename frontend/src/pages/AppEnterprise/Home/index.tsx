ebb38b36e9cf049622c0e82c190857bc7271e255
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-03-11 21:02:51 -0300

    feat: home-layout-done

--- frontend/src/pages/AppEnterprise/Home/index.tsx
+++ frontend/src/pages/AppEnterprise/Home/index.tsx
@@ -4,14 +4,29 @@ import {
     HomePhoto, 
     HomeSubTitle, 
     HomeTitle,
-    HomeContent
+    HomeContent,
+    EnterpriseInfoContainer,
+    EnterpriseCard,
+    EnterpriseLogo,
+    EnterpriseDetails,
+    EnterpriseLabel,
+    EnterpriseTotal,
+    EmployeeCard,
+    EmployeeIcon,
+    StockSummary,
+    StockCard,
+    QuickActions,
+    ActionButton,
+    EnterpriseDetailsText
 } from "./styles";
 
 import { EmployeeLengthWarn } from "../../../components/EmployeeLengthWarn";
 import Image from '../../../assets/enterprise-stock.jpg';
+import Logo from '../../../assets/SyncStock.png';
 
 export default function HomeEnterprise() {
     const length: number = 0;
+    
     return (
         <Container>
             <HomeContentContainer>
@@ -23,11 +38,41 @@ export default function HomeEnterprise() {
                 <HomePhoto src={Image} />
 
                 <HomeContent>
-                    
+                    <EnterpriseInfoContainer>
+
+                        <EnterpriseCard>
+                            <EnterpriseLogo src={Logo} alt="Logo da Empresa" />
+                            <EnterpriseDetails>
+                                <EnterpriseDetailsText>
+                                    SyncStock {/* nome da empresa */}
+                                </EnterpriseDetailsText>
+                            </EnterpriseDetails>
+                        </EnterpriseCard>
+
+                        <EmployeeCard>
+                            <EmployeeIcon>👥</EmployeeIcon>
+                            <EnterpriseDetails>
+                                <EnterpriseLabel>Funcionários Cadastrados</EnterpriseLabel>
+                                <EnterpriseTotal>35</EnterpriseTotal>
+                            </EnterpriseDetails>
+                        </EmployeeCard>
+                    </EnterpriseInfoContainer>
+
                 </HomeContent>
 
+                <StockSummary>
+                    <StockCard>📦 Produtos: 120</StockCard>
+                    <StockCard>📂 Categorias: 10</StockCard>
+                    {/* <StockCard>⚠️ Baixo Estoque: 5</StockCard> */}
+                </StockSummary>
+
+                <QuickActions>
+                    <ActionButton>➕ Adicionar Produto</ActionButton>
+                    <ActionButton>👥 Cadastrar Funcionário</ActionButton>
+                    <ActionButton>📂 Gerenciar Categorias</ActionButton>
+                </QuickActions>
+
             </HomeContentContainer>
-            
         </Container>
     );
 }
