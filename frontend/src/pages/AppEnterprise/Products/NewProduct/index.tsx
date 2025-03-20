9ba7806aa320da204ee37f32d3896a1fc824c32e
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-03-19 21:49:24 -0300

    feat: New-Product-Page-created

---
+++ frontend/src/pages/AppEnterprise/Products/NewProduct/index.tsx
@@ -0,0 +1,13 @@
+import { Container, ProductContentContainer, ProductsTitle, ProductsTitleAddContainer } from "../styles";
+
+export default function NewProductEnterprise() {
+    return (
+        <Container>
+            <ProductContentContainer>
+                <ProductsTitleAddContainer>
+                    <ProductsTitle>Adicionar Produtos</ProductsTitle>
+                </ProductsTitleAddContainer>
+            </ProductContentContainer>
+        </Container>
+    );
+}
