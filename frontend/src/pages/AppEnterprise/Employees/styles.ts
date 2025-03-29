c3dce662d22fd2e1dc34f0fbc9a1de5c1bdb4a65
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-03-29 15:20:21 -0300

    fix: employee-container-width

--- frontend/src/pages/AppEnterprise/Employees/styles.ts
+++ frontend/src/pages/AppEnterprise/Employees/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.article`
-    
+    display: flex;
+    justify-content: center;
 `;
 
 export const EmployeesContentContainer = styled.div`
