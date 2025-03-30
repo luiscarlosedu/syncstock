cdb76f4ffd65856f8da07bec504b3cca6a406241
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-03-30 18:56:21 -0300

    feat: feat: paste-structure

---
+++ frontend/src/pages/AppEnterprise/Employees/NewEmployee/index.tsx
@@ -0,0 +1,11 @@
+import { Container, EmployeesContentContainer } from "../styles";
+
+export default function NewEmployeeEnterprise() {
+    return (
+        <Container>
+            <EmployeesContentContainer>
+                
+            </EmployeesContentContainer>
+        </Container>
+    );
+}
