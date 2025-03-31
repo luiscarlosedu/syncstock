edb5f9a941929f67f4ca85dc633706644fe3a8cd
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-03-30 21:28:05 -0300

    feat: navigate-home

--- frontend/src/pages/AppEnterprise/Employees/NewEmployee/index.tsx
+++ frontend/src/pages/AppEnterprise/Employees/NewEmployee/index.tsx
@@ -1,11 +1,23 @@
 import { Container, EmployeesContentContainer } from "../styles";
+import { EmployeeTitle, EmployeeTitleContainer, RegisterEmployeeContent } from "./styles";
 
 export default function NewEmployeeEnterprise() {
     return (
         <Container>
             <EmployeesContentContainer>
-                
+                <EmployeeTitleContainer>
+                    <EmployeeTitle>Cadastrar funcionário</EmployeeTitle>
+                </EmployeeTitleContainer>
+
+                <RegisterEmployeeContent>
+                    
+                </RegisterEmployeeContent>
             </EmployeesContentContainer>
         </Container>
     );
 }
+
+/*
+Insira o e-mail do funcionário que deseja cadastrar na empresa. 
+                    Ele receberá um convite para acessar a plataforma.
+*/
--- frontend/src/pages/AppEnterprise/Employees/NewEmployee/styles.ts
+++ frontend/src/pages/AppEnterprise/Employees/NewEmployee/styles.ts
@@ -1,5 +1,17 @@
 import styled from "styled-components";
 
-export const Container = styled.article`
+export const EmployeeTitleContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+`;
+
+export const EmployeeTitle = styled.h1`
+    font-size: 20px;
+    font-weight: 600;
+`;
+
+export const RegisterEmployeeContent = styled.div`
     
 `;
