b711e6a6f489d9e554c1f805d94f6e70f5783efc
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-04-21 15:17:12 -0300

    feat: MyStoreHead

--- frontend/src/pages/AppEnterprise/MyStore/index.tsx
+++ frontend/src/pages/AppEnterprise/MyStore/index.tsx
@@ -1,11 +1,50 @@
-import { Container, MyStoreContentContainer } from "./styles";
+import { ChangeInfo, ChangeInfoItem, Container, MyStoreContentContainer, MyStoreHead, MyStoryHeadInfo, StoreImage, StoryBtn, StoryBtnArea, StoryTitleName } from "./styles";
+
+import Image from '../../../assets/syncstock-white.png';
 
 export default function MyStoreEnterprise() {
     return (
         <Container>
             <MyStoreContentContainer>
+                <MyStoreHead>
+                    <StoreImage src={Image} />
+                    <MyStoryHeadInfo>
+                        <StoryTitleName>
+                            SyncStock
+                        </StoryTitleName>
+                        <StoryBtnArea>
+                            <StoryBtn>Editar</StoryBtn>
+                            <StoryBtn>Configurar</StoryBtn>
+                        </StoryBtnArea>
+                    </MyStoryHeadInfo>
+                </MyStoreHead>
 
+                <ChangeInfo>
+                    <ChangeInfoItem>Informações</ChangeInfoItem>
+                    <ChangeInfoItem>Status</ChangeInfoItem>
+                    <ChangeInfoItem>Funcionários</ChangeInfoItem>
+                </ChangeInfo>
             </MyStoreContentContainer>
         </Container>
     );
 };
+
+/*
+model Empresa {
+  id String @id @default(uuid())
+  nome String
+  cnpj String @unique
+  email String @unique
+  senha String
+  foto String?
+  endereco String?
+  telefone String?
+  createdAt DateTime? @default(now())
+  updatedAt DateTime @updatedAt
+
+  @@map("empresas")
+  funcionarios Funcionario[] 
+  categorias Categoria[]
+  produtos Produto[]
+}
+*/
--- frontend/src/pages/AppEnterprise/MyStore/styles.ts
+++ frontend/src/pages/AppEnterprise/MyStore/styles.ts
@@ -8,8 +8,71 @@ export const Container = styled.article`
 export const MyStoreContentContainer = styled.main`
     width: 100%;
     max-width: 1100px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
 
     @media screen and (min-width: 1800px) {
         max-width: 1300px;
     }
+`;
+
+export const MyStoreHead = styled.div`
+    background-color: #fff;
+    border-radius: 8px;
+    display: flex;
+    gap: 15px;
+    padding: 20px;
+    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.05);
+`;
+
+export const StoreImage = styled.img`
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    object-fit: cover;
+    background-color: #202020;
+    border: 2px solid #202020;
+`;
+
+export const MyStoryHeadInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 10px;
+`;
+
+export const StoryTitleName = styled.h2`
+    font-size: 25px;
+`;
+
+export const StoryBtnArea = styled.div`
+    display: flex;
+    gap: 5px;
+`;
+
+export const StoryBtn = styled.button`
+    background-color: #202020;
+    color: white;
+    padding: 5px 10px;
+    border-radius: 7px;
+    cursor: pointer;
+    transition: all .3s;
+
+    &:hover {
+        background-color: #454545;
+    }
+`;
+
+export const ChangeInfo = styled.div`
+    background-color: white;
+    display: flex;
+    gap: 20px;
+    padding: 5px 20px;
+`;
+
+export const ChangeInfoItem = styled.button`
+    border: none;
+    background-color: white;
+    cursor: pointer;
 `;
