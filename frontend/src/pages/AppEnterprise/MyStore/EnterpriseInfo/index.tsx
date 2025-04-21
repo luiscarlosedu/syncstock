89f3a4a0236d92b56a54b37b915081593990786d
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-04-21 17:43:05 -0300

    feat: mystore-info-done

--- frontend/src/pages/AppEnterprise/MyStore/EnterpriseInfo/index.tsx
+++ frontend/src/pages/AppEnterprise/MyStore/EnterpriseInfo/index.tsx
@@ -1,9 +1,31 @@
-import { Container } from "./styles";
+import { Container, InfoItem, InfoItemName, InfoItemValue } from "./styles";
 
 export function EnterpriseInfo() {
     return (
         <Container>
-            <h1>Page Info gerais</h1>
+            <InfoItem>
+                <InfoItemName>CNPJ:</InfoItemName>
+                <InfoItemValue>12.345.678/0001-99</InfoItemValue>
+            </InfoItem>
+            <InfoItem>
+                <InfoItemName>Email:</InfoItemName>
+                <InfoItemValue>synstock@gmail.com</InfoItemValue>
+            </InfoItem>
+
+            <InfoItem>
+                <InfoItemName>Telefone:</InfoItemName>
+                <InfoItemValue>(88)997876890</InfoItemValue>
+            </InfoItem>
+            <InfoItem>0
+                <InfoItemName>Endereço:</InfoItemName>
+                <InfoItemValue>Av. das Empresas, 123 - SP</InfoItemValue>
+            </InfoItem>
+            <InfoItem>
+                <InfoItemName>Desde:</InfoItemName>
+                <InfoItemValue>01/01/2024</InfoItemValue>
+            </InfoItem>
+
+            
         </Container>
     );
 }
