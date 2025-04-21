2b665d9a477c6128faa0eb86add60a040ee8902b
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-04-21 17:15:27 -0300

    fix: container-props-styles

--- frontend/src/pages/AppEnterprise/MyStore/EnterpriseInfo/styles.ts
+++ frontend/src/pages/AppEnterprise/MyStore/EnterpriseInfo/styles.ts
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  
+    background-color: #fff;
+    padding: 10px 20px;
+    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.05);
+    border-radius: 8px;
 `;
