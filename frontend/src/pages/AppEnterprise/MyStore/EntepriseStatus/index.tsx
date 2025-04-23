685654ca194bf422f9c2a43b45c2a4fcb3230e83
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-04-22 21:43:26 -0300

    feat: status-card

--- frontend/src/pages/AppEnterprise/MyStore/EntepriseStatus/index.tsx
+++ frontend/src/pages/AppEnterprise/MyStore/EntepriseStatus/index.tsx
@@ -1,9 +1,22 @@
-import { Container } from "./styles";
+import { FaUsers } from "react-icons/fa";
+import { Container, StatusCard, StatusCardContent, StatusCardIcon, StatusTitle, StatusValue } from "./styles";
 
 export function EnterpriseStatus() {
     return (
         <Container>
-            <h1>Page Status</h1>
+            <StatusCard>
+                <StatusCardIcon>
+                    <FaUsers size={30} />
+                </StatusCardIcon>
+                <StatusCardContent>
+                    <StatusValue>8</StatusValue>
+                    <StatusTitle>Funcionários</StatusTitle>
+                </StatusCardContent>
+            </StatusCard>
+
+            <StatusCard>
+                
+            </StatusCard>
         </Container>
     );
 }
--- frontend/src/pages/AppEnterprise/MyStore/EntepriseStatus/styles.ts
+++ frontend/src/pages/AppEnterprise/MyStore/EntepriseStatus/styles.ts
@@ -3,4 +3,43 @@ import styled from "styled-components";
 export const Container = styled.div`
     background-color: #fff;
     padding: 10px 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    border-radius: 8px;
+`;
+
+export const StatusCard = styled.div`
+    background-color: #F1F1F1;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    padding: 12px 16px;
+    border-radius: 8px;
+    width: 100%;
+    box-shadow: 0 2px 6px rgba(0,0,0,0.05);
+`;
+
+export const StatusCardIcon = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    height: 40px;
+`;
+
+export const StatusCardContent = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const StatusValue = styled.span`
+    font-weight: bold;
+    font-size: 18px;
+    color: #202020;
+`;
+
+export const StatusTitle = styled.span`
+    font-size: 14px;
+    color: #555;
 `;
