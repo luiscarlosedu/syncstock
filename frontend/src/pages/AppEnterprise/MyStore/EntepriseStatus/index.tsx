0fa0c227e22248608f6927a53e51e28746a0176c
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-04-23 18:27:20 -0300

    feat: Status-card-category

--- frontend/src/pages/AppEnterprise/MyStore/EntepriseStatus/index.tsx
+++ frontend/src/pages/AppEnterprise/MyStore/EntepriseStatus/index.tsx
@@ -1,13 +1,14 @@
 import { FaUsers } from "react-icons/fa";
 import { Container, StatusCard, StatusCardContent, StatusCardIcon, StatusTitle, StatusValue } from "./styles";
 import { IoMdPricetag } from "react-icons/io";
+import { MdCategory } from "react-icons/md";
 
 export function EnterpriseStatus() {
     return (
         <Container>
             <StatusCard>
                 <StatusCardIcon>
-                    <FaUsers size={30} />
+                    <FaUsers size={25} />
                 </StatusCardIcon>
                 <StatusCardContent>
                     <StatusValue>8</StatusValue>
@@ -17,11 +18,21 @@ export function EnterpriseStatus() {
 
             <StatusCard>
                 <StatusCardIcon>
-                    <IoMdPricetag size={30} />
+                    <IoMdPricetag size={25} />
                 </StatusCardIcon>
                 <StatusCardContent>
                     <StatusValue>140</StatusValue>
                     <StatusTitle>Produtos</StatusTitle>
+                </StatusCardContent>                
+            </StatusCard>
+
+            <StatusCard>
+                <StatusCardIcon>
+                    <MdCategory size={25} />
+                </StatusCardIcon>
+                <StatusCardContent>
+                    <StatusValue>10</StatusValue>
+                    <StatusTitle>Categorias</StatusTitle>
                 </StatusCardContent>                
             </StatusCard>
         </Container>
