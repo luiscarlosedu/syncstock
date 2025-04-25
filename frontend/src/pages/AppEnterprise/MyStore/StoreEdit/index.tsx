35bc2d93838829ce707d3a8730d7a7c69bf599ac
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-04-24 21:31:32 -0300

    feat: store-edit-content-container

--- frontend/src/pages/AppEnterprise/MyStore/StoreEdit/index.tsx
+++ frontend/src/pages/AppEnterprise/MyStore/StoreEdit/index.tsx
@@ -1,10 +1,13 @@
 import { MyStoreHeader } from "../../../../components/my-store-header";
-import { Container } from "./styles";
+import { Container, StoreEditContentContainer } from "./styles";
 
 export default function StoreEdit() {
     return (
         <Container>
-            <MyStoreHeader />
+            <StoreEditContentContainer>
+                <MyStoreHeader />
+
+            </StoreEditContentContainer>
         </Container>
     );
 }
