935957490a6545f5e172139bb647aa83c3c6379b
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-04-26 16:08:24 -0300

    feat: StoreEditContent-done

--- frontend/src/pages/AppEnterprise/MyStore/StoreEdit/index.tsx
+++ frontend/src/pages/AppEnterprise/MyStore/StoreEdit/index.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import { MyStoreHeader } from "../../../../components/my-store-header";
 import { Container, StoreEditContent, StoreEditContentContainer, StoreEditInput, StoreEditInputContainer, StoreEditLabel, StoreEditSubmit, StoreEditTwoInputsContainer } from "./styles";
 
 export default function StoreEdit() {
+    const [empresa, setEmpresa] = useState({
+        nome: 'Empresa Exemplo Ltda',
+        cnpj: '00.000.000/0001-00',
+        // email: 'contato@empresa.com',
+        // senha: '',
+        endereco: 'Rua Exemplo, 123 - Centro',
+        telefone: '8899999-9999',
+    });
+
     return (
         <Container>
             <StoreEditContentContainer>
@@ -16,6 +26,8 @@ export default function StoreEdit() {
                             id="iname"
                             name="name"
                             type="text"
+                            value={empresa.nome}
+                            onChange={(e) => setEmpresa({...empresa, nome: e.target.value})}
                         />
                     </StoreEditInputContainer>
 
@@ -26,30 +38,36 @@ export default function StoreEdit() {
                             id="iaddress"
                             name="address"
                             type="text"
+                            value={empresa.endereco}
+                            onChange={(e) => setEmpresa({...empresa, endereco: e.target.value})}
                         />
                     </StoreEditInputContainer>
 
                     <StoreEditTwoInputsContainer>
                         <StoreEditInputContainer>
                             <StoreEditLabel htmlFor="iphone">Telefone</StoreEditLabel>
-                            <StoreEditInput
-                                placeholder="88997876900"
-                                id="iphone"
-                                name="phone"
-                                type="number"
-                            />
+                                <StoreEditInput
+                                    placeholder="88997876900"
+                                    id="iphone"
+                                    name="phone"
+                                    type="tel"
+                                    value={empresa.telefone}
+                                    onChange={(e) => setEmpresa({...empresa, telefone: e.target.value})}
+                                />
                         </StoreEditInputContainer>
 
                         <StoreEditInputContainer>
                             <StoreEditLabel htmlFor="icnpj">CNPJ</StoreEditLabel>
-                            <StoreEditInput
-                                placeholder="12.345.678/0001-99"
-                                id="icnpj"
-                                name="cnpj"
-                                type="number"
-                            />
-                        </StoreEditInputContainer>
-                    </StoreEditTwoInputsContainer>
+                                <StoreEditInput
+                                    placeholder="12.345.678/0001-99"
+                                    id="icnpj"
+                                    name="cnpj"
+                                    type="text"
+                                    value={empresa.cnpj}
+                                    onChange={(e) => setEmpresa({...empresa, cnpj: e.target.value})}
+                                />
+                            </StoreEditInputContainer>
+                        </StoreEditTwoInputsContainer>
                     
                     <StoreEditSubmit>
                         Atualizar
