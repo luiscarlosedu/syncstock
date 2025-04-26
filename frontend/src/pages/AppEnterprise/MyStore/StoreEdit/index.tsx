e65ee2bb3021be77ed5f1eda7b5bcadee942f4f7
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-04-26 15:36:00 -0300

    feat: no-styled-store-edit-input-container

--- frontend/src/pages/AppEnterprise/MyStore/StoreEdit/index.tsx
+++ frontend/src/pages/AppEnterprise/MyStore/StoreEdit/index.tsx
@@ -1,11 +1,22 @@
 import { MyStoreHeader } from "../../../../components/my-store-header";
-import { Container, StoreEditContentContainer } from "./styles";
+import { Container, StoreEditContent, StoreEditContentContainer, StoreEditInput, StoreEditInputContainer, StoreEditLabel } from "./styles";
 
 export default function StoreEdit() {
     return (
         <Container>
             <StoreEditContentContainer>
                 <MyStoreHeader />
+
+                <StoreEditContent>
+                    <StoreEditInputContainer>
+                        <StoreEditLabel htmlFor="iname">Nome da empresa</StoreEditLabel>
+                        <StoreEditInput 
+                            placeholder="Nome da Empresa"
+                            id="iname"
+                            name="name"
+                        />
+                    </StoreEditInputContainer>
+                </StoreEditContent>
 
             </StoreEditContentContainer>
         </Container>
--- frontend/src/pages/AppEnterprise/MyStore/StoreEdit/styles.ts
+++ frontend/src/pages/AppEnterprise/MyStore/StoreEdit/styles.ts
@@ -8,8 +8,31 @@ export const Container = styled.article`
 export const StoreEditContentContainer = styled.main`
     width: 100%;
     max-width: 1100px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
 
     @media screen and (min-width: 1800px) {
         max-width: 1300px;
     }
+`;
+
+export const StoreEditContent = styled.form`
+    background: #fff;   
+    width: 100%;
+    padding: 10px;
+`;
+
+export const StoreEditInputContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const StoreEditLabel = styled.label`
+    
+`;
+
+export const StoreEditInput = styled.input`
+    
 `;
