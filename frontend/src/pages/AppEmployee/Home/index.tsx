c7d814ba6bf356b76a077d69c5b279a6b51bcecc
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-04-28 21:00:16 -0300

    feat: create-employee-route-guard

--- frontend/src/pages/AppEmployee/Home/index.tsx
+++ frontend/src/pages/AppEmployee/Home/index.tsx
@@ -4,7 +4,7 @@ export default function HomeEmployee() {
     return (
         <Container>
             <HomeContentContainer>
-
+                
             </HomeContentContainer>
         </Container>
     );
---
+++ frontend/src/routes/EmployeeRouteGuard.tsx
@@ -0,0 +1,38 @@
+import { Outlet } from "react-router";
+
+export function EmployeeRouteGuard() {
+    const funcionario = {
+        nome: "arnaldo",
+        email: "eduardo@teste.com",
+        employed: true
+    };
+
+    if(!funcionario.employed) {
+        console.log("error");
+        return;
+    }
+
+    return <Outlet />
+}
+
+/*
+
+model Funcionario {
+  id String @id @default(uuid())
+  nome String
+  email String @unique
+  senha String
+  employed Boolean @default(false)
+  foto String?
+  
+  // movimentacoes Movimentacao[]
+
+  createdAt DateTime? @default(now())
+  updatedAt DateTime @updatedAt
+
+  empresa Empresa? @relation(fields: [empresa_id], references: [id])
+
+  @@map("funcionarios")
+  empresa_id String? 
+}
+*/
