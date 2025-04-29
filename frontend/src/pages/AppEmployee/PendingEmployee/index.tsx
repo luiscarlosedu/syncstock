d8d77c4ad245a164c37289d88323af372cc5b0f5
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-04-28 21:19:08 -0300

    feat: adding-header-on-PendingEmployee-page

--- frontend/src/pages/AppEmployee/PendingEmployee/index.tsx
+++ frontend/src/pages/AppEmployee/PendingEmployee/index.tsx
@@ -1,9 +1,10 @@
 import { Container } from "./styles";
+import { Header } from "../../../components/header";
 
 export default function PendingEmployee() {
     return (
         <Container>
-            
+            <Header type="employee" />
         </Container>
     );
 }
