88cebc92d1865abbafed4fa5fc322d4cb855a917
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-05-07 10:21:17 -0300

    feat: create-by-mystore-header

--- frontend/src/pages/AppEmployee/MyAccountPending/index.tsx
+++ frontend/src/pages/AppEmployee/MyAccountPending/index.tsx
@@ -1,14 +1,84 @@
+import { useNavigate } from "react-router";
+import { MyStoreHead, MyStoryHeadInfo, StoreImage, StoryBtn, StoryBtnArea, StoryTitleName } from "../../../components/my-store-header/styles";
 import { PendingHeader } from "../../../components/pending-header";
 import { Container, MyAccountContentContainer } from "./styles";
 
+interface EmployeeProps {
+    name: string;
+    type: "employee" | "enterprise"
+    employed: boolean;
+    image: string;
+}
+
 export default function MyAccountPending() {
+    const navigate = useNavigate();
+
+    const funcionario: EmployeeProps = {
+        name: 'Victor Shaoles',
+        employed: true,
+        type: 'employee',
+        image: "https://github.com/victorrxyv.png"
+    }
+        
     return (
         <>
             <PendingHeader/>
             <Container>
-                
                 <MyAccountContentContainer>
-                    <p>akdiawjdwaknd</p>
+                    <MyStoreHead>
+                        <StoreImage src={funcionario.image} />
+                        <MyStoryHeadInfo>
+                            <StoryTitleName>
+                                {funcionario.name}
+                            </StoryTitleName>
+        
+                            {location.pathname === '/funcionario/pendente/detalhes' && (
+                                <StoryBtnArea>
+                                    <StoryBtn
+                                        onClick={() => navigate('/funcionario/pendente/detalhes/editar')}
+                                    >
+                                        Editar
+                                    </StoryBtn>
+                                        <StoryBtn
+                                        onClick={() => navigate('/funcionario/pendente/detalhes/configurar')}
+                                    >
+                                        Configurar
+                                    </StoryBtn>
+                                </StoryBtnArea>
+                            )}
+        
+                            {location.pathname === '/empresa/detalhes/editar' && (
+                                <StoryBtnArea>
+                                    <StoryBtn
+                                        onClick={() => navigate('/empresa/detalhes')}
+                                    >
+                                        Detalhes
+                                    </StoryBtn>
+                                        <StoryBtn
+                                        onClick={() => navigate('/empresa/detalhes/configurar')}
+                                    >
+                                        Configurar
+                                    </StoryBtn>
+                                </StoryBtnArea>
+                            )}
+        
+                            {location.pathname === '/empresa/detalhes/configurar' && (
+                                <StoryBtnArea>
+                                    <StoryBtn
+                                        onClick={() => navigate('/empresa/detalhes')}
+                                    >
+                                        Detalhes
+                                    </StoryBtn>
+                                        <StoryBtn
+                                        onClick={() => navigate('/empresa/detalhes/editar')}
+                                    >
+                                        Editar
+                                    </StoryBtn>
+                                </StoryBtnArea>
+                            )}
+                            
+                        </MyStoryHeadInfo>
+                    </MyStoreHead>
                 </MyAccountContentContainer>
             </Container>
         </>
