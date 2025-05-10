86e5c906475673d4e54491fa9f3b422ae329eb0f
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-05-10 16:15:27 -0300

    fix: info-on-pending

--- frontend/src/pages/AppEmployee/MyAccountPending/MyAccountPendingInfo/index.tsx
+++ frontend/src/pages/AppEmployee/MyAccountPending/MyAccountPendingInfo/index.tsx
@@ -4,25 +4,20 @@ export default function MyAccountPendinginfo() {
     return (
         <Container>
             <InfoItem>
-                <InfoItemName>CNPJ:</InfoItemName>
-                <InfoItemValue>12.345.678/0001-99</InfoItemValue>
+                <InfoItemName>Nome:</InfoItemName>
+                <InfoItemValue>Luís Eduardo</InfoItemValue>
             </InfoItem>
             <InfoItem>
                 <InfoItemName>Email:</InfoItemName>
-                <InfoItemValue>synstock@gmail.com</InfoItemValue>
-            </InfoItem>
-
-            <InfoItem>
-                <InfoItemName>Telefone:</InfoItemName>
-                <InfoItemValue>(88)997876890</InfoItemValue>
-            </InfoItem>
-            <InfoItem>
-                <InfoItemName>Endereço:</InfoItemName>
-                <InfoItemValue>Av. das Empresas, 123 - SP</InfoItemValue>
+                <InfoItemValue>eduardotop@gmail.com</InfoItemValue>
             </InfoItem>
             <InfoItem>
                 <InfoItemName>Desde:</InfoItemName>
                 <InfoItemValue>01/01/2024</InfoItemValue>
+            </InfoItem>
+            <InfoItem>
+                <InfoItemName>Empresa:</InfoItemName>
+                <InfoItemValue>Nenhuma</InfoItemValue>
             </InfoItem>
         </Container>
     );
