2870caa861c1ef8c1558dc83f59020850dd303a1
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-05-30 09:49:32 -0300

    feat: width-input-column-EmployeeStatus

--- frontend/src/pages/AppEnterprise/Employees/styles.ts
+++ frontend/src/pages/AppEnterprise/Employees/styles.ts
@@ -54,6 +54,15 @@ export const EmployeesStatus = styled.div`
     padding: 12px;
     border-radius: 6px;
     box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.05);
+
+    @media screen and (max-width: 768px){
+        flex-direction: column;
+        gap: 4px;
+
+        input {
+            width: 100%;
+        }
+    }
 `;
 
 export const EmployeesTotalContainer = styled.div`
