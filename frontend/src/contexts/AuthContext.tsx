00f4893edd7a467aebc6bf5a02f89f22f65cfff7
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-06-07 15:02:53 -0300

    feat: pre-auth-context

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,19 +1,19 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 
 interface AuthProviderProps {
     children: ReactNode;
 }
 
 interface AuthContextProps {
-  signed: boolean;
-  user: UserProps | null;
-  loadingAuth: boolean;
-  loading: boolean;
-  signUpEnterPrise: (email: string, cnpj: string, senha: string) => Promise<void>;
-  signUpEmployee: (email: string, senha: string) => Promise<void>;
-  signInEnterprise: (email: string, senha: string, cnpj: string) => Promise<void>;
-  signInEmployee: (email: string, senha: string) => Promise<void>;
-  signOut: () => void;
+    signed: boolean;
+    user: UserProps | null;
+    //   loadingAuth: boolean;
+    //   loading: boolean;
+    signUpEnterprise: (email: string, cnpj: string, senha: string) => /*Promise<void>*/ void;
+    signUpEmployee: (email: string, senha: string) => /*Promise<void>*/ void;
+    signInEnterprise: (email: string, cnpj: string, senha: string, ) => /*Promise<void>*/ void;
+    signInEmployee: (email: string, senha: string) => /*Promise<void>*/ void;
+    signOut: () => void;
 }
 
 interface UserProps {
@@ -28,10 +28,54 @@ interface UserProps {
 export const AuthContext = createContext({} as AuthContextProps);
 
 export default function AuthProvider({children}: AuthProviderProps) {
-    // const [user, setUser] = useState<UserProps | null>(null);
+    const [user, setUser] = useState<UserProps | null>(null);
+
+    useEffect(() => {
+        const testUser: UserProps = {
+            id: "2",
+            nome: "Jonas",
+            email: "jonastop@gmail.com",
+            tipo: "funcionario",
+            employed: true,
+        }    
+
+        setUser(testUser);
+    }, []);
+
+    function signUpEnterprise(email: string, cnpj: string, senha: string) {
+        console.log("Cadastrou uma empresa");
+        console.log(email, cnpj, senha)
+    }
+    
+    function signInEnterprise(email: string, cnpj: string, senha: string) {
+        console.log("Entrou uma empresa");
+        console.log(email, cnpj, senha);
+    }
+
+    function signUpEmployee(email: string, senha: string) {
+        console.log("Cadastrou um funcionario");
+        console.log(email, senha);
+    }
+
+    function signInEmployee(email: string, senha: string) {
+        console.log("Entrou um funcionario");
+        console.log(email, senha);
+    }
+
+    function signOut() {
+        console.log("desconectou");
+    }
 
     return (
-        <AuthContext.Provider value={{}}>
+        <AuthContext.Provider value={{
+            signed: !!user,
+            user,
+            signInEnterprise,
+            signUpEnterprise,
+            signInEmployee,
+            signUpEmployee,
+            signOut,
+        }}>
             {children}
         </AuthContext.Provider>
     );
