f29ba43f0ba6e8f7bb4bbdc8c48fdd0584d7d0da
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-06-16 22:30:58 -0300

    feat: crud-my-store-header

--- frontend/src/components/my-store-header/index.tsx
+++ frontend/src/components/my-store-header/index.tsx
@@ -1,16 +1,25 @@
-import { useNavigate, useLocation } from "react-router";
+import { useNavigate, useLocation, Navigate } from "react-router";
 import { MyStoreHead, MyStoryHeadInfo, StoreImage, StoryBtn, StoryBtnArea, StoryTitleName } from "./styles";
-
-import Image from '../../assets/syncstock-white.png';
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export function MyStoreHeader() {
+    const { user } = useContext(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
+
+    if(!user) {
+        return <Navigate to={"/"} replace />
+    };
 
     return (
         <>
             <MyStoreHead>
-                <StoreImage src={Image} />
+                <StoreImage src={
+                    user?.foto
+                        ? `${import.meta.env.VITE_API_URL}/files/${user.foto}`
+                        : `https://ui-avatars.com/api/?name=${encodeURIComponent(user.nome)}&background=202020&color=fff`
+                } />
                 <MyStoryHeadInfo>
                     <StoryTitleName>
                         SyncStock
