d26be1aa0c4b95ffb7b62580a535afb8be3cc52a
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-06-16 22:27:54 -0300

    feat: crud-enterprise-info

--- frontend/src/pages/AppEnterprise/MyStore/EnterpriseInfo/index.tsx
+++ frontend/src/pages/AppEnterprise/MyStore/EnterpriseInfo/index.tsx
@@ -1,28 +1,45 @@
+import { useContext } from "react";
 import { Container, InfoItem, InfoItemName, InfoItemValue } from "./styles";
+import { AuthContext } from "../../../../contexts/AuthContext";
+import { Navigate } from "react-router";
 
 export function EnterpriseInfo() {
+    const { user } = useContext(AuthContext);
+
+    if(!user) {
+        return <Navigate to={"/"} replace />
+    }
+
+    function formattedDate(user_createdAt: string) {
+        const createdDate = new Date(user_createdAt);
+        const formatted = createdDate.toLocaleDateString("pt-BR");
+        return formatted;
+    }
+
+    console.log(user.cnpj);
+    
     return (
         <Container>
             <InfoItem>
                 <InfoItemName>CNPJ:</InfoItemName>
-                <InfoItemValue>12.345.678/0001-99</InfoItemValue>
+                <InfoItemValue>{user?.cnpj}</InfoItemValue>
             </InfoItem>
             <InfoItem>
                 <InfoItemName>Email:</InfoItemName>
-                <InfoItemValue>synstock@gmail.com</InfoItemValue>
+                <InfoItemValue>{user.email}</InfoItemValue>
             </InfoItem>
 
             <InfoItem>
                 <InfoItemName>Telefone:</InfoItemName>
-                <InfoItemValue>(88)997876890</InfoItemValue>
+                <InfoItemValue>(88)997876890 - editar</InfoItemValue>
             </InfoItem>
             <InfoItem>
                 <InfoItemName>Endereço:</InfoItemName>
-                <InfoItemValue>Av. das Empresas, 123 - SP</InfoItemValue>
+                <InfoItemValue>Av. das Empresas, 123 - SP - editar</InfoItemValue>
             </InfoItem>
             <InfoItem>
                 <InfoItemName>Desde:</InfoItemName>
-                <InfoItemValue>01/01/2024</InfoItemValue>
+                <InfoItemValue>{formattedDate(user.createdAt)}</InfoItemValue>
             </InfoItem>
 
             
