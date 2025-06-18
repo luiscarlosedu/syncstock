8a735a3770f3c376fa8ad45cd226fbcaa50ad088
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-06-17 21:45:02 -0300

    feat: get-status-enterprise

--- frontend/src/pages/AppEnterprise/MyStore/EntepriseStatus/index.tsx
+++ frontend/src/pages/AppEnterprise/MyStore/EntepriseStatus/index.tsx
@@ -2,8 +2,32 @@ import { FaUsers } from "react-icons/fa";
 import { Container, StatusCard, StatusCardContent, StatusCardIcon, StatusTitle, StatusValue } from "./styles";
 import { IoMdPricetag } from "react-icons/io";
 import { MdCategory } from "react-icons/md";
+import { useEffect, useState } from "react";
+import api from "../../../../services/api";
 
 export function EnterpriseStatus() {
+    const [employeesTotal, setEmployeesTotal] = useState(0);
+    const [productsTotal, setProductsTotal] = useState(0);
+    const [categoriesTotal, setCategoriesTotal] = useState(0);
+
+    useEffect(() => {
+        loadStorage();
+    }, []);
+
+    async function loadStorage() {
+        try {
+            const response = await api.get("/enterprise/status");
+            const { employeesTotal, productsTotal, categoriesTotal } = response.data;
+
+            setEmployeesTotal(employeesTotal);
+            setProductsTotal(productsTotal);
+            setCategoriesTotal(categoriesTotal);
+        } catch (err) {
+            console.log("[ERRO] ", err);
+        }
+
+    }
+
     return (
         <Container>
             <StatusCard>
@@ -11,7 +35,7 @@ export function EnterpriseStatus() {
                     <FaUsers size={25} />
                 </StatusCardIcon>
                 <StatusCardContent>
-                    <StatusValue>8</StatusValue>
+                    <StatusValue>{employeesTotal}</StatusValue>
                     <StatusTitle>Funcionários</StatusTitle>
                 </StatusCardContent>
             </StatusCard>
@@ -21,7 +45,7 @@ export function EnterpriseStatus() {
                     <IoMdPricetag size={25} />
                 </StatusCardIcon>
                 <StatusCardContent>
-                    <StatusValue>140</StatusValue>
+                    <StatusValue>{productsTotal}</StatusValue>
                     <StatusTitle>Produtos</StatusTitle>
                 </StatusCardContent>                
             </StatusCard>
@@ -31,7 +55,7 @@ export function EnterpriseStatus() {
                     <MdCategory size={25} />
                 </StatusCardIcon>
                 <StatusCardContent>
-                    <StatusValue>10</StatusValue>
+                    <StatusValue>{categoriesTotal}</StatusValue>
                     <StatusTitle>Categorias</StatusTitle>
                 </StatusCardContent>                
             </StatusCard>
