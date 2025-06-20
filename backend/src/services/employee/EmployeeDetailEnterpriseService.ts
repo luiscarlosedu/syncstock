3902640c1842dae326d4dedad124b61a20b909b9
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-06-20 13:10:10 -0300

    feat: employee-detail-enterprise

--- backend/src/services/employee/EmployeeDetailEnterpriseService.ts
+++ backend/src/services/employee/EmployeeDetailEnterpriseService.ts
@@ -16,13 +16,38 @@ export class EmployeeDetailEnterpriseService {
                 telefone: true,
                 endereco: true,
                 createdAt: true,
+                funcionarios: {
+                    select: {
+                        id: true,
+                        nome: true,
+                        email: true,
+                        foto: true,
+                        createdAt: true,
+                    },
+                },
             },
         });
 
         if(!enterprise) {
             throw new Error("[ERROR] Empresa não encontrada!");
-        }
+        };
 
-        return enterprise;
+        const productsCount = await prismaClient.produto.count({
+            where: {
+                empresa_id: enterprise_id,
+            },
+        });
+
+        const categoriesCount = await prismaClient.categoria.count({
+            where: {
+                empresa_id: enterprise_id,
+            },
+        });
+
+        return {
+            ...enterprise,
+            productsCount,
+            categoriesCount,
+        };
     }
 }
