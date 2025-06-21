340f451fed4aafdbec5b9b6cc10fb220179a812b
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-06-21 17:17:59 -0300

    fix: remove-new-category-employee

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -29,7 +29,6 @@ import FAQEnterprise from "./pages/AppEnterprise/FAQ";
 
 import HomeEmployee from "./pages/AppEmployee/Home";
 import CategoriesEmployee from "./pages/AppEmployee/Categories";
-import NewCategoryEmployee from "./pages/AppEmployee/Categories/NewCategory";
 import ProductsEmployee from "./pages/AppEmployee/Products";
 import EnterpriseEmployee from "./pages/AppEmployee/Enterprise";
 import AboutEmployee from "./pages/AppEmployee/About";
@@ -148,10 +147,6 @@ const router = createBrowserRouter([
           {
             path: '/funcionario/categorias',
             element: <CategoriesEmployee />
-          },
-          {
-            path: '/funcionario/categorias/criar',
-            element: <NewCategoryEmployee />
           },
           {
             path: '/funcionario/produtos',
