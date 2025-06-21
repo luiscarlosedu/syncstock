a4d29b2459233e5f6ffcf239ce24284015a9e086
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-06-20 22:45:47 -0300

    fix: return-add-employees-count

--- backend/src/services/enterprise/GetDetailEnterpriseService.ts
+++ backend/src/services/enterprise/GetDetailEnterpriseService.ts
@@ -45,6 +45,7 @@ export class GetDetailEnterpriseService {
 
         return {
             ...enterprise,
+            employeesCount,
             productsCount,
             categoriesCount,
         };
