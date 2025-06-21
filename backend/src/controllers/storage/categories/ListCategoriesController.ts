31fa929df221e6e4c62b75c3c0d31b720fc57665
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-06-21 16:45:27 -0300

    fix: req-body-receive-enterprise_id

--- backend/src/controllers/storage/categories/ListCategoriesController.ts
+++ backend/src/controllers/storage/categories/ListCategoriesController.ts
@@ -3,7 +3,7 @@ import { ListCategoriesService } from "../../../services/storage/categories/List
 
 export class ListCategoriesController {
     async handle(req: Request, res: Response) {
-        const enterprise_id = req.user_id;
+        const { enterprise_id } = req.body;
         const listCategoriesService = new ListCategoriesService();
         const categories = await listCategoriesService.execute({enterprise_id});
 
