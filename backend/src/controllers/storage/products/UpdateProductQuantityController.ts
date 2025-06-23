e4c8446b51c8be74fd3eb1f8b12273ffb59849a0
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-06-23 10:41:49 -0300

    feat: update-product-done

--- backend/src/controllers/storage/products/UpdateProductQuantityController.ts
+++ backend/src/controllers/storage/products/UpdateProductQuantityController.ts
@@ -4,11 +4,17 @@ import { Request, Response } from "express";
 export class UpdateProductQuantityController {
     async handle(req: Request, res: Response) {
         const { id } = req.params;
-        const { quantidade } = req.body;
+        const { quantidade, tipo } = req.body;
+
+        if (!id || !quantidade || !tipo) {
+            throw new Error("[ERROR] Você não preencheu todos os campos!");
+        };
 
         const updateProductQuantityService = new UpdateProductQuantityService();
         const updateProduct = await updateProductQuantityService.execute({
-            id, quantidade: Number(quantidade),
+            id, 
+            quantidade: Number(quantidade),
+            tipo: tipo === 'entrada' ? "entrada" : "saida",
         });
 
         res.json(updateProduct);
