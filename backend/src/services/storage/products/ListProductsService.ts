43a49ebae2e5fb55b096e6d6183065278e1f420c
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-06-23 07:34:16 -0300

    feat: return-products-get

--- backend/src/services/storage/products/ListProductsService.ts
+++ backend/src/services/storage/products/ListProductsService.ts
@@ -9,7 +9,20 @@ export class ListProductsService {
         const products = await prismaClient.produto.findMany({
             where: {
                 empresa_id: enterprise_id,
-            }
+            },
+            select: {
+                id: true,
+                nome: true,
+                preco: true,
+                quantidade: true,
+                categoria: {
+                    select: {
+                        id: true,
+                        nome: true,
+                    },
+                },
+                foto: true,
+            },
         });
 
         return products;
