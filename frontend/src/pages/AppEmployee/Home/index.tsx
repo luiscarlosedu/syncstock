7045f67846bf171c83af86a3fe25d989124ef353
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-07-04 17:10:01 -0300

    fix: isloading-false-home-employee

--- frontend/src/pages/AppEmployee/Home/index.tsx
+++ frontend/src/pages/AppEmployee/Home/index.tsx
@@ -40,7 +40,7 @@ export default function HomeEmployee() {
             } catch (err) {
                 console.log("[ERRO]", err);
             } finally {
-                setIsLoading(true);
+                setIsLoading(false);
             };
         };
 
