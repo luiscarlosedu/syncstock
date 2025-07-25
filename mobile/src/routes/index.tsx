fc4c00dc75d26a148f7622d766b16facf5d31c98
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-07-25 11:44:17 -0300

    feat: index-routes

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,3 +1,25 @@
+import AuthRoutes from "./auth.routes";
+import EmployeeRoutes from "./employee.routes";
+import EnterpriseRoutes from "./enterprise.routes";
+
 export default function Routes() {
-    return;
+    const user = {
+        nome: 'José',
+        tipo: 'empresa'
+    };
+    const signed = true; 
+        
+    if (!signed) {
+        return <AuthRoutes />
+    };
+
+    if (user?.tipo === 'empresa') {
+        return <EnterpriseRoutes />
+    };
+
+    if (user?.tipo === 'funcionario') {
+        return <EmployeeRoutes />
+    };
+
+    return <AuthRoutes />
 }
