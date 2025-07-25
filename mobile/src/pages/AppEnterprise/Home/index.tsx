4a2f78004373d35d6fca3900ad8ae2fab0aecd3b
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-07-25 11:29:43 -0300

    feature/routes-auth-app

---
+++ mobile/src/pages/AppEnterprise/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { Container } from "./styles";
+
+export default function HomeEnterprise() {
+    return (
+        <Container>
+            <Text>Página Home Enterprise</Text>
+        </Container>
+    )
+}
---
+++ mobile/src/pages/Select/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+import { Container } from "./styles";
+
+export default function Select() {
+    return (
+        <Container>
+            <Text>Hello World!</Text>
+            <Text>Página Select!</Text>
+        </Container>
+    );
+}
