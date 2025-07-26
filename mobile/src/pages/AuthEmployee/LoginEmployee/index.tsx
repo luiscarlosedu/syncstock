b3d2a0b907d28cfce58a78d9609939c6861ff8db
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-07-26 15:33:36 -0300

    feat: login-employee-page

--- mobile/src/pages/AuthEmployee/LoginEmployee/index.tsx
+++ mobile/src/pages/AuthEmployee/LoginEmployee/index.tsx
@@ -1,21 +1,61 @@
-import { Button, Text } from "react-native";
+import { ActivityIndicator, Text } from "react-native";
 import { AuthLayout } from "../../../components/layouts/auth-layout";
 import { useNavigation } from "@react-navigation/native";
 import { AuthRoutesList } from "../../../routes/auth.routes";
-import { Container } from "./styles";
+import { Container, HorizontalRow, HorizontalRowText, Hr, Input, LoginContent, LoginContentText, LoginFooter, LoginFooterLink, LoginFooterText, LoginForm, LoginFormSubmit, LoginInputContainer, LoginLabel, LoginSubTitle, LoginTitle } from "./styles";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { AuthHeader } from "../../../components/auth-header";
+import { useState } from "react";
 
 type NavigationProps = NativeStackNavigationProp<AuthRoutesList, "LoginEmployee">
 
 export default function LoginEmployee() {
     const navigation = useNavigation<NavigationProps>();
+    const [loading, setLoading] = useState(false);
 
     return (
         <AuthLayout>
             <Container>
-                <Text>Página Login Employee</Text>
-                <Button title="Ir se registrar" onPress={() => navigation.navigate("RegisterEmployee")} />
-                <Button title="Ir para home" onPress={() => navigation.goBack()} />
+                <AuthHeader />
+                <LoginContent>
+                    <LoginContentText>
+                        <LoginTitle>Entrar como Funcionário</LoginTitle>
+                        <LoginSubTitle>Que bom te ver de novo!</LoginSubTitle>
+                    </LoginContentText>
+
+                    <LoginForm>
+                        <LoginInputContainer>
+                            <LoginLabel>Email</LoginLabel>
+                            <Input placeholder="testeteste@gmail.com" keyboardType="email-address" />
+                        </LoginInputContainer>
+
+                        <LoginInputContainer>
+                            <LoginLabel>Senha</LoginLabel>
+                            <Input placeholder="***************" secureTextEntry />
+                        </LoginInputContainer>
+
+                        <LoginFormSubmit disabled={loading}>
+                            {loading ? (
+                                <ActivityIndicator color="#fff" />
+                            ) : (
+                                <Text style={{ color: "#fff" }}>Entrar</Text>
+                            )}
+                        </LoginFormSubmit>
+                    </LoginForm>
+
+                    <HorizontalRow>
+                        <Hr />
+                        <HorizontalRowText>ou</HorizontalRowText>
+                        <Hr />
+                    </HorizontalRow>
+
+                    <LoginFooter>
+                        <LoginFooterText>
+                            Não possui uma conta de funcionário?{" "}
+                            <LoginFooterLink onPress={() => navigation.navigate("RegisterEmployee")} >Crie-a agora!</LoginFooterLink>
+                        </LoginFooterText>
+                    </LoginFooter>
+                </LoginContent>
             </Container>
         </AuthLayout>
     );
