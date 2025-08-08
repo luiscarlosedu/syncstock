809b2c8d6df0f206dd25ae7fe9e794d85a1af4c4
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-08-08 16:38:46 -0300

    feat: header-done

--- mobile/src/components/app-header/index.tsx
+++ mobile/src/components/app-header/index.tsx
@@ -1,10 +1,36 @@
-import { Text } from "react-native";
-import { Container } from "./styles";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Container, HeaderLogo, Logo, Title, MyEnterprise, MyEnterpriseText, MyEnterpriseImage } from "./styles";
+import { StatusBar } from "react-native";
 
 export function AppHeader() {
     return (
-        <Container>
-            <Text>SyncStock</Text>
-        </Container>
+        <>
+            <StatusBar 
+                backgroundColor="#1A1A1A"
+                barStyle="light-content"
+            />
+            <SafeAreaView>
+                <Container>
+                    <HeaderLogo>
+                        <Logo 
+                            source={require('../../assets/syncstock-white.png')} 
+                            resizeMode="cover"
+                        />
+                        <Title>
+                            SyncStock
+                        </Title>
+                    </HeaderLogo>
+                    <MyEnterprise activeOpacity={0.7}>
+                        <MyEnterpriseText>
+                            Empresa
+                        </MyEnterpriseText>
+                        <MyEnterpriseImage
+                            source={{ uri: 'https://github.com/luiscarlosedu.png' }}
+                            resizeMode="cover"
+                        />
+                    </MyEnterprise>
+                </Container>
+            </SafeAreaView>
+        </>
     );
 }
