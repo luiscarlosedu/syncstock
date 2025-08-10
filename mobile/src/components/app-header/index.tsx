cdec2f3b4589ebbd00a1f7fb06183d674df35768
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-08-10 15:59:39 -0300

    feat: header-structure

--- mobile/src/components/app-header/index.tsx
+++ mobile/src/components/app-header/index.tsx
@@ -1,36 +1,30 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Container, HeaderLogo, Logo, Title, MyEnterprise, MyEnterpriseText, MyEnterpriseImage } from "./styles";
 import { StatusBar } from "react-native";
+import { Container, HeaderLogo, Logo, Title, MyEnterprise, MyEnterpriseText, MyEnterpriseImage } from "./styles";
 
 export function AppHeader() {
     return (
-        <>
+        <SafeAreaView style={{ backgroundColor: "#1A1A1A" }} edges={['top']}>
             <StatusBar 
                 backgroundColor="#1A1A1A"
                 barStyle="light-content"
             />
-            <SafeAreaView>
-                <Container>
-                    <HeaderLogo>
-                        <Logo 
-                            source={require('../../assets/syncstock-white.png')} 
-                            resizeMode="cover"
-                        />
-                        <Title>
-                            SyncStock
-                        </Title>
-                    </HeaderLogo>
-                    <MyEnterprise activeOpacity={0.7}>
-                        <MyEnterpriseText>
-                            Empresa
-                        </MyEnterpriseText>
-                        <MyEnterpriseImage
-                            source={{ uri: 'https://github.com/luiscarlosedu.png' }}
-                            resizeMode="cover"
-                        />
-                    </MyEnterprise>
-                </Container>
-            </SafeAreaView>
-        </>
+            <Container>
+                <HeaderLogo>
+                    <Logo
+                        source={require('../../assets/syncstock-white.png')}
+                        resizeMode="cover"
+                    />
+                    <Title>SyncStock</Title>
+                </HeaderLogo>
+                <MyEnterprise activeOpacity={0.7}>
+                    <MyEnterpriseText>Empresa</MyEnterpriseText>
+                    <MyEnterpriseImage
+                        source={{ uri: 'https://github.com/luiscarlosedu.png' }}
+                        resizeMode="cover"
+                    />
+                </MyEnterprise>
+            </Container>
+        </SafeAreaView>
     );
 }
