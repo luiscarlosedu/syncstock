366ca8563d93d0a4ef9a1c18ad9dff11758489a5
luiscarlosedu <eduardo.luis070408@gmail.com>
2026-02-24 14:26:10 -0300

    fix: Fixing request problems

--- backend/src/controllers/storage/categories/ListCategoriesController.ts
+++ backend/src/controllers/storage/categories/ListCategoriesController.ts
@@ -3,9 +3,10 @@ import { ListCategoriesService } from "../../../services/storage/categories/List
 
 export class ListCategoriesController {
     async handle(req: Request, res: Response) {
-        const { enterprise_id } = req.body;
+        const { enterprise_id } = req.query;
+        
         const listCategoriesService = new ListCategoriesService();
-        const categories = await listCategoriesService.execute({enterprise_id});
+        const categories = await listCategoriesService.execute({enterprise_id: enterprise_id as string});
 
         res.json(categories);
     }
