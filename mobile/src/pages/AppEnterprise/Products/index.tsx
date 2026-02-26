497410bd6381c4c2ea3313e2793bf44e8d6abaa7
luiscarlosedu <eduardo.luis070408@gmail.com>
2026-02-26 12:56:40 -0300

    feat: home-page-done

--- mobile/src/pages/AppEnterprise/Products/index.tsx
+++ mobile/src/pages/AppEnterprise/Products/index.tsx
@@ -1,12 +1,51 @@
-import { Text } from "react-native";
-import { Container } from "./styles";
+import { useState } from "react";
+import { FlatList, TextInput, View } from "react-native";
 import { EnterpriseLayout } from "../../../components/layouts/enterprise-layout";
+import { Container, ProductContentContainer, ProductsTitle, ProductsTitleAddContainer } from "./styles";
+
+const FIXED_PRODUCTS = [
+    {
+        id: "1",
+        nome: "Parafuso Sextavado M8",
+        quantidade: "150",
+        categoria_nome: "Fixadores",
+        foto: "",
+    },
+    {
+        id: "2",
+        nome: "Chave de Fenda Phillips",
+        quantidade: "40",
+        categoria_nome: "Ferramentas",
+        foto: "",
+    },
+    {
+        id: "3",
+        nome: "Luva de Segurança",
+        quantidade: "80",
+        categoria_nome: "EPIs",
+        foto: "",
+    },
+    {
+        id: "4",
+        nome: "Tinta Spray Preta 400ml",
+        quantidade: "25",
+        categoria_nome: "Acabamentos",
+        foto: "",
+    },
+];
 
 export default function ProductsEnterprise() {
+
     return (
         <EnterpriseLayout>
             <Container>
-                <Text>Página Products Enterprise</Text>
+                <ProductContentContainer>
+                    <ProductsTitleAddContainer>
+                        <ProductsTitle>Produtos</ProductsTitle>
+                    </ProductsTitleAddContainer>
+
+                    
+                </ProductContentContainer>
             </Container>
         </EnterpriseLayout>
     );
--- mobile/src/pages/AppEnterprise/Products/styles.ts
+++ mobile/src/pages/AppEnterprise/Products/styles.ts
@@ -1,6 +1,24 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
     flex: 1;
-    background-color: #F1F1F1;
+    background-color: #f1f1f1;
+`;
+
+export const ProductContentContainer = styled.View`
+    padding: 16px;
+    padding-bottom: 40px;
+    gap: 12px;
+`;
+
+export const ProductsTitleAddContainer = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const ProductsTitle = styled.Text`
+    font-size: 22px;
+    font-weight: 700;
+    color: #1a1a1a;
 `;
